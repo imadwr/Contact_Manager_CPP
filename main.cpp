cd821f7ef6a01ce7f2947166b21095972c058aa6
imad <imadouairem96@gmail.com>
2021-12-17 17:49:42 +0100

    add main menu

--- main.cpp
+++ main.cpp
@@ -5,8 +5,47 @@ using namespace std;
 
 int main(){
 	
-	test a;
-	a.print();
+	int choice;
+	
+	
+	do {
+		cout << "\n\t**********Contact Manager Application**********";
+		cout << "\n\n";
+		cout << "\t\tMain Menu:";
+		cout << "\n\t\t----------------------";
+		cout << "\n\t\t[1] Add a new Contact";
+		cout << "\n\t\t[2] Edit a Contact";
+		cout << "\n\t\t[3] Show all Contacts";
+		cout << "\n\t\t[4] Search for Contact";
+		cout << "\n\t\t[5] Delete a Contact";
+		cout << "\n\t\t[0] Exit";
+		cout << "\n\t\t----------------------";
+		cout << "\n\t\tWhat do you want to do: ";
+		cin >> choice;
+		
+		switch(choice){
+			case 1:
+				//add contact
+				break;
+			case 2:
+				//edit contact
+				break;
+			case 3:
+				//show contacts
+				break;
+			case 4:
+				//shearch for cointact
+				break;
+			case 5:
+				//delete contact
+				break;
+			case 0:
+				break;
+			default: 
+			continue;
+		}
+  
+	}while(choice != 0);
 	
 	return 0;
 }
